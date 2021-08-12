52b9178318e64cdd069f94cf1398e01a3024354b
Almaz Gimaev <almazgimaev@gmail.com>
2021-08-12 11:04:25 +0300

    new game 'Snake' added  (gameover doesn`t work)

--- app/app.js
+++ app/app.js
@@ -411,123 +411,416 @@ addEventListener('click', (event) => {
 })
   
 
+pnts.textBaseline = 'bottom'
+pnts.font = '50px FuturaLightC'
+pnts.fillStyle = '#b9b8b8'
+pnts.fillText('Даша, ', 20, 70)
+pnts.fillText('у тебя', 20, 130)
+pnts.font = '50px Futura New Medium'
+pnts.fillText(' всё', 140, 130)
+pnts.font = '50px FuturaLightC'
+pnts.fillText('получится!', 20, 190)
+
+
 
 // ____________________________________________________
 // отскакивающий мячик
 
 
-let Ball = function() {
+let Ball = function(num) {
   this.x = 100;
   this.y = 100;
-  this.xSpeed = -5;
-  this.ySpeed = 4
+  this.xSpeed = -4;
+  this.ySpeed = 4;
+  this.radius = num
 }
-let ballArea = document.getElementById('ball')
-let bll = ballArea.getContext('2d')
 
-
-let circle = function(x,y, radius, fillCircle) {
-  bll.beginPath();
-  bll.fillStyle = '#DC3545';
-  bll.strokeStyle = '#DC3545';
-  bll.arc(x,y, radius, 0, 2 * Math.PI, false)
+let circle = function(area, x,y, radius, fillCircle) {
+  area.beginPath();
+  area.fillStyle = '#DC3545';
+  area.strokeStyle = '#DC3545';
+  area.arc(x,y, radius, 0, 2 * Math.PI, false)
   if(fillCircle) {
-    bll.fill()
+    area.fill()
   } else {
-    bll.stroke()
+    area.stroke()
   }
 }
 
-Ball.prototype.draw = function() {
-  circle(this.x, this.y, 12, true)
-  circleNew(this.x, this.y, 12, true)
+Ball.prototype.draw = function(area) {
+  circle(area, this.x, this.y, this.radius, true)
 }
- 
+
 Ball.prototype.move = function() {
   this.x += this.xSpeed;
   this.y += this.ySpeed
 }
 
+
 Ball.prototype.checkCollision = function() {
-  if(this.x - 10 < 0 || this.x + 10 > 400) {
+  if(this.x - this.radius < 0 || this.x + this.radius > 400) {
     this.xSpeed = -this.xSpeed
   }
-  if(this.y - 10 < 0 || this.y + 10 > 200) {
+  if(this.y - this.radius < 0 || this.y + this.radius > 200) {
     this.ySpeed = -this.ySpeed
   }
 }
 
-const ball = new Ball()
+Ball.prototype.control = function (button) {
+  if (button === 'up' && this.ySpeed > -15) {
+    this.ySpeed = -Math.abs(this.ySpeed)
+  } else  if (button === 'down' && this.ySpeed < 15) {
+    this.ySpeed = Math.abs(this.ySpeed)
+  }
+  if (button === 'left' && this.xSpeed > -15) {
+    this.xSpeed = -Math.abs(this.xSpeed)
+  } else if (button === 'right' && this.xSpeed < 15) {
+    this.xSpeed = Math.abs(this.xSpeed)
+  }
+  if (button === 'zoomIn') {
+    this.radius += 1
+  } else if (button === 'zoomOut') {
+    this.radius -= 1
+  }
+  if (button === 'speedDown') {
+    this.xSpeed > 0 ? this.xSpeed -= 1 : this.xSpeed += 1
+    this.ySpeed > 0 ? this.ySpeed -= 1 : this.ySpeed += 1
+  } else if (button === 'speedUp') {
+    this.xSpeed >= 0 ? this.xSpeed += 1 : this.xSpeed -= 1
+    this.ySpeed >= 0 ? this.ySpeed += 1 : this.ySpeed -= 1
+  }
+}
 
-setInterval(() => {
-  bll.clearRect(0,0,400,200)
-
-  ball.draw()
-  ball.move()
-  ball.checkCollision()
-
-}, 30);
-
+Ball.prototype.startStop = function(button) {
+  if(this.x - this.radius <= 1 || this.x + this.radius >= 399 || this.y - this.radius <= 1 || this.y + this.radius > 199) {
+    return
+  }
+  if (button === 'stop') {
+    this.xSpeed = 0;
+    this.ySpeed = 0
+  } 
+  if (button === 'start') {
+    this.xSpeed = Math.floor(Math.random() * 6);
+    this.ySpeed = - Math.floor(Math.random() * 4)
+  }
+}
 
 
 
 
 // ____________________________________________________
-  // отскакивающий мячик с управлением
+// отскакивающий мячик
+
+let ballArea = document.getElementById('ball')
+let bll = ballArea.getContext('2d')
+
+
+
+
+const ball = new Ball(5)
+
+
+
+setInterval(() => {
+  bll.clearRect(0,0,400,200)
+  
+  ball.draw(bll)
+  ball.move()
+  ball.checkCollision()
+  
+}, 30);
+
+$('body').keydown(function(event) {
+  if (Object.keys(keyActions).includes(event.keyCode + '')) {
+    event.preventDefault()
+  }
+  let button = keyActions[event.keyCode]
+  ball.startStop(button)
+})
+
+
+
+// ____________________________________________________
+// отскакивающий мячик с управлением
 
 let gameBallArea = document.getElementById('gameBall')
 let gbll = gameBallArea.getContext('2d')
 
-let circleNew = function (x,y,radius, fillCircle) {
-  gbll.beginPath()
-  gbll.fillStyle = '#DC3545';
-  gbll.strokeStyle = '#DC3545';
-  gbll.arc(x,y, radius, 0, 2 * Math.PI, false)
-  if(fillCircle) {
-    gbll.fill()
-  } else {
-    gbll.stroke()
-  }
-}
-Ball.prototype.setDirection = function (direction) {
-  if (direction === 'up' && this.ySpeed > -15) {
-    this.ySpeed -= 3
-  } else  if (direction === 'down' && this.ySpeed < 15) {
-    this.ySpeed += 3
-  }
-  if (direction === 'left' && this.xSpeed > -15) {
-    this.xSpeed -= 3;
-  } else if (direction === 'right' && this.xSpeed < 15) {
-    this.xSpeed += 3;
-  }
-  if (direction === 'stop') {
-    this.xSpeed = 0;
-    this.ySpeed = 0
-  }
-}
-const gameBall = new Ball()
+const gameBall = new Ball(12)
 
 const keyActions = {
+  17: 'start',
   32: 'stop',
   37: 'left',
   38: 'up',
   39: 'right',
-  40: 'down'
+  40: 'down',
+  97: 'zoomIn',
+  96: 'zoomOut',
+  98: 'speedDown',
+  99: 'speedUp'
 }
 
 $('body').keydown(function(event) {
   if (Object.keys(keyActions).includes(event.keyCode + '')) {
     event.preventDefault()
   }
-  let direction = keyActions[event.keyCode]
-  ball.setDirection(direction)
+  let button = keyActions[event.keyCode]
+  gameBall.control(button)
+  gameBall.startStop(button)
 })
 
 setInterval(() => {
   gbll.clearRect(0,0,400,200)
 
-  ball.draw()
-  ball.move()
-  ball.checkCollision()
+  gameBall.draw(gbll)
+  gameBall.move()
+  gameBall.checkCollision()
 
 }, 30);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// __________________________________________________________________
+// Змейка
+
+let snakeArea = document.querySelector('#snake-area')
+const snk = snakeArea.getContext('2d')
+
+
+const widthAreaS = snakeArea.width // 5 строк размеров
+const heightAreaS = snakeArea.height
+const blockSize = 10
+const widthInBlocks = widthAreaS / blockSize
+const heightInBlocks = heightAreaS / blockSize
+
+let score = 0 //устанавливаем счет игры
+
+
+const drawBorder = function() { //функция, чтобы наирсовать рамку
+  snk.fillStyle = 'Gray'
+  snk.fillRect(0, 0, widthAreaS, blockSize)
+  snk.fillRect(0, heightAreaS - blockSize, widthAreaS, blockSize)
+  snk.fillRect(0,0, blockSize, heightAreaS)
+   snk.fillRect(widthAreaS - blockSize, 0, blockSize, heightAreaS)
+}
+
+const drawScore = function(color) {  //функция, чтобы вывести счет игры
+  snk.font = '40px Futura New'
+  snk.fillStyle = color || '#DC3545'
+  snk.textAlign = 'left'
+  snk.textBaseline = 'top'
+  snk.fillText(`Счет: ${score}`,15,15)
+
+}
+
+const gameOver = function() {  //функция, чтобы остановить игру при проигрыше
+  clearTimeout(timeoutId)
+  snk.fillStyle = 'rgba(0,0,0,0.3)'
+  snk.fillRect(0,0,800,800)
+  snk.font = '120px Futura New'
+  snk.textAlign = 'center'
+  snk.fillStyle = '#DC3545'
+  snk.textBaseline = 'middle'
+  snk.fillText(`Вы проиграли`, widthAreaS / 2, heightAreaS / 2)
+  drawScore('rgba(1,1,1,0.8)')
+
+  $('body').keydown(function(event) {
+    let newDirection = directions[event.keyCode]
+    if(newDirection !== undefined) {
+      snake.setDirection(newDirection)
+    }
+  })
+}
+
+// let circle = function(area, x,y, radius, fillCircle) { //функция, чтобы наирсовать круг
+//   area.beginPath();
+//   area.fillStyle = '#DC3545';
+//   area.strokeStyle = '#DC3545';
+//   area.arc(x,y, radius, 0, 2 * Math.PI, false)
+//   if(fillCircle) {
+//     area.fill()
+//   } else {
+//     area.stroke()
+//   }
+// }
+// эта функция была написана в предыдущих играх (здесь лишь копия)
+
+
+// --------------------------------------------
+// Создаем блок (будем использовать этот конструктор для других конструкторов)
+let Block = function(col, row)  {
+  this.col = col;
+  this.row = row
+}
+
+Block.prototype.drawSquare = function(color) {
+  let x = this.col * blockSize;
+  let y = this.row * blockSize;
+  snk.fillStyle = color;
+  snk.fillRect(x, y, blockSize, blockSize)
+}
+
+Block.prototype.drawRound = function(color) {
+  let centerX = this.col * blockSize + blockSize / 2;
+  let centerY = this.row * blockSize + blockSize / 2;
+  snk.fillStyle = color;
+  circle(snk, centerX, centerY, blockSize / 2, true)
+}
+
+Block.prototype.equal = function(otherBlock) {
+  return this.col === otherBlock.col && this.row === otherBlock.row
+}
+
+
+// Создаем змейку--------------------------------------------
+let Snake = function () {
+  this.segments = [
+    new Block(11, 11),
+    new Block(10, 11),
+    new Block(9, 11)
+  ];
+
+  this.direction = 'right';
+  this.nextDirection = 'right'
+}
+
+Snake.prototype.draw = function() {
+  for (let i = 0; i < this.segments.length; i++) {
+    this.segments[i].drawSquare('#63A80A')
+    
+  }
+}
+
+Snake.prototype.move = function() {
+  let head = this.segments[0];
+  let newHead;
+
+  this.direction = this.nextDirection;
+
+  if (this.direction === "right") {
+    newHead = new Block(head.col + 1, head.row)
+  } else if (this.direction === "down") {
+    newHead = new Block(head.col, head.row + 1)
+  } else if (this.direction === "left") {
+    newHead = new Block(head.col - 1, head.row)
+  } else if (this.direction === "up") {
+    newHead = new Block(head.col, head.row - 1)
+  }
+
+  if (this.checkCollision(newHead)) {
+    gameOver();
+    return
+  }
+
+  this.segments.unshift(newHead) 
+
+  if(newHead.equal(apple.position)) {
+    score++ 
+    animationTime -=2
+    apple.move() //двигаем яблоко куда-то
+  } else {
+    this.segments.pop() //если не нашли яблоко удаляем с конца 1 элемент,т.к. в начале доабвили
+  }
+}
+
+Snake.prototype.checkCollision = function(head) {
+  let leftCollision = (head.col === 0);
+  let topCollision = (head.row === 0);
+  let rightCollision = (head.col === widthInBlocks - 1);
+  let bottomCollision = (head.row === heightInBlocks - 1);
+
+  let wallCollision = leftCollision || topCollision || rightCollision || bottomCollision
+  let selfCollision = false
+  
+  for (let i = 1; i < this.segments.length; i++) {
+    if (head.equal(this.segments[i])) {
+    
+      
+      selfCollision = true
+    }
+  }
+  return wallCollision || selfCollision
+}
+
+Snake.prototype.setDirection = function(newDirection) {
+  if(this.direction === 'up' && newDirection === 'down') {
+    return
+  }
+  if(this.direction === 'right' && newDirection === 'left') {
+    return
+  }
+  if(this.direction === 'down' && newDirection === 'up') {
+    return
+  }
+  if(this.direction === 'left' && newDirection === 'right') {
+    return
+  }
+
+  this.nextDirection = newDirection
+}
+
+
+// Создаем яблоко--------------------------------------------
+let Apple = function () {
+  this.position = new Block(15,15)
+}
+
+Apple.prototype.draw = function() {
+  this.position.drawRound('green')
+}
+Apple.prototype.move = function() {
+  let randomCol = Math.floor(Math.random() * (widthInBlocks - 2)) + 1;
+  let randomRow = Math.floor(Math.random() * (heightInBlocks - 2)) + 1
+  this.position = new Block(randomCol, randomRow)
+}
+
+
+let snake = new Snake() //создаем исходную змею
+let apple = new Apple() //создаем первое яблоко
+
+let timeoutId
+
+let animationTime = 100
+const gameLoop = function() {
+  snk.clearRect(0, 0, widthAreaS, heightAreaS)
+  drawScore()
+  snake.move() //запускаем движ змеи
+  snake.draw() //рисуем ее по новой
+  apple.draw() //рисуем созданное яблоко
+  drawBorder()
+  let timeoutId = setTimeout(gameLoop, animationTime);
+}
+
+gameLoop()
+
+// const intervalId = setInterval(() => {
+  
+// }, 50);
+
+
+const directions = {  //клавиши для управления
+  65: 'left',
+  87: 'up',
+  68: 'right',
+  83: 'down',
+}
+
+
+
+$('body').keydown(function(event) {
+  let newDirection = directions[event.keyCode]
+  if(newDirection !== undefined) {
+    snake.setDirection(newDirection)
+  }
+})
